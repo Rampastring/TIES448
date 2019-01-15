ac80ea58e17c10c3a0f31ceb814743f19b4c633b
jsaarimaa <jose.saarimaa@gmail.com>
2019-01-15 16:16:01 +0200

    Add exception for syntax errors

--- Hassembler/Hassembler/Visitor/VisitException.cs
+++ Hassembler/Hassembler/Visitor/VisitException.cs
@@ -8,4 +8,18 @@ namespace Hassembler
         {
         }
     }
+
+    class SyntaxError : Exception
+    {
+        private int Line { get; }
+        private int Column { get; }
+        private string F_name { get; }
+
+        public SyntaxError(int line, int column, string f_name ,string message) : base (message)
+        {
+            this.Line = line;
+            this.Column = column;
+            this.F_name = f_name;
+        }
+    }
 }
