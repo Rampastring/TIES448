cba664bb27ca49310dae982c0e7a9e65e6c810fd
M.K <monikynsi@gmail.com>
2019-01-31 16:03:46 +0200

    Add unit tests

--- Hassembler/Hassembler/Program.cs
+++ Hassembler/Hassembler/Program.cs
@@ -9,13 +9,13 @@ namespace Hassembler
     {
         static void Main(string[] args)
         {
-            // string testiohjelma = "a=1+1+1*3\r\nf=2+4\r\ng=2*(4+5)\r\nh=2*4+5\r\nr=f+a\r\nbn=2+r\r\n";
+            //string testiohjelma = "a=1+1+1*3\r\nf=2+4\r\ng=2*(4+5)\r\nh=2*4+5\r\nr=f+a\r\nbn=2+r\r\n";
             // string testiohjelma = "a=0-4\r\nb=2 + if a then 4+4 else 2 + 3";
             // string testiohjelma = "fabulous a b = a + b\ny=4 + fabulous 2 3\n";
-            //string testiohjelma = "fabulous a = if a > 0 then (a + fabulous (a-1)) else 0";
+            // string testiohjelma = "fabulous a = if a > 0 then (a + fabulous (a-1)) else 0";
             string testiohjelma = "f = 2 == 2\ng = True == True\nh = False == False\n" +
                 "i = True == False\nj = True != False\nk = True != True\n";
-
+            
             Console.WriteLine("Haskell-- (Haskell-minus-minus) interpreter");
 
             Hassembler hassembler = new Hassembler();
---
+++ Hassembler/HassemblerTests/InterpreterTests.cs
@@ -0,0 +1,129 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HassemblerTests
+{
+    [TestClass]
+    public class InterpreterTests
+    {
+        Hassembler.Hassembler hassembler = new Hassembler.Hassembler();
+
+        /// <summary>
+        /// Tests basic arithmetic.
+        /// Input: 1+2*3
+        /// <returns>
+        /// 7
+        /// </returns>
+        /// </summary>
+        //[TestMethod]
+        //public void Arith()
+        //{
+        //    hassembler.ParseCode("1+2*3");
+        //    Assert.Fail();
+        // currently the interpreter does not handle basic arithmetics outside functions 
+        // }
+
+    /// <summary>
+    /// Tests basic arithmetic function.
+    /// Source code: f = 1+2*3
+    /// Input: f
+    /// <returns>
+    /// f = 7
+    /// </returns>
+    /// </summary>
+    [TestMethod]
+        public void ArithFun()
+        {
+            hassembler.ParseCode("f = 1+2*3");
+            Assert.AreEqual("f = 7", hassembler.CallFunction("f", new List<object>()));
+        }
+
+        /// <summary>
+        /// Tests booleans
+        /// Source code: f = 2 == 2
+        /// Input: f
+        /// <returns>
+        /// f = True
+        /// </returns>
+        /// </summary>
+        [TestMethod]
+        public void BoolTest()
+        {
+            hassembler.ParseCode("f = 2 == 2");
+            Assert.AreEqual("f = True", hassembler.CallFunction("f", new List<object>()));
+        }
+        /// <summary>
+        /// Tests the getter for all functions
+        /// Source code: 
+        /// a=1+1+1*3
+        /// f=2+4
+        /// <returns>
+        /// a f
+        /// </returns> 
+        /// </summary>
+        [TestMethod]
+        public void GetFuncs()
+        {
+            hassembler.ParseCode("a=1+1+1*3\r\nf=2+4");
+            string str = "";
+            foreach (Hassembler.Function f in hassembler.GetFunctions)
+                str += f.Name + ' ';
+            Assert.AreEqual("a f ", str);
+        }
+
+        /// <summary>
+        /// Test function call with parameters
+        /// Source code:
+        /// "fabulous a b = a + b
+        ///  y=4 + fabulous 2 3
+        ///  "
+        ///  Input: y
+        ///  <returns>
+        ///  
+        ///  </returns>
+        /// </summary>
+        [TestMethod]
+        public void FunCallWithParameters()
+        {
+            hassembler.ParseCode("fabulous a b = a + b\ny=4 + fabulous 2 3\n");
+            Assert.AreEqual("y = 9", hassembler.CallFunction("y", new List<object>()));
+        }
+        /// <summary>
+        /// Tests function references to other functions
+        /// Input: 
+        /// a=3
+        /// f=a+4
+        /// <returns>
+        /// Found functions: a f
+        /// </returns> 
+        /// </summary>
+        [TestMethod]
+        public void FuncWithRef()
+        {
+            hassembler.ParseCode("a=3\r\nf=a+4");
+            string str = "";
+            foreach (Hassembler.Function f in hassembler.GetFunctions)
+                str += f.Name + ' ';
+            Assert.AreEqual("a f ", str);
+        }
+
+        /// <summary>
+        /// Tests recursive function with fibonacci (giving end condition)
+        /// Source code: 
+        /// "f x y end = if y < end then f y (x+y) end else y"
+        /// Input: 
+        /// f 1 1 10
+        /// <returns>
+        /// f = 13
+        /// </returns> 
+        /// </summary>
+        [TestMethod]
+        public void Recursion()
+        {
+            hassembler.ParseCode("f x y end = if y < end then f y (x+y) end else y");
+            Assert.AreEqual("f = 13", hassembler.CallFunction("f", new List<object>() { 1,1,10 } ));
+        }
+
+
+    }
+}
