55d587127a4ba4fd4a1238d09152048ecdbef32c
Rampastring <rampastring@outlook.com>
2019-02-05 18:42:40 +0200

    Clean up Program.cs

--- Hassembler/Hassembler/Program.cs
+++ Hassembler/Hassembler/Program.cs
@@ -9,17 +9,12 @@ namespace Hassembler
     {
         static void Main(string[] args)
         {
-            //string testiohjelma = "a=1+1+1*3\r\nf=2+4\r\ng=2*(4+5)\r\nh=2*4+5\r\nr=f+a\r\nbn=2+r\r\n";
-            // string testiohjelma = "a=0-4\r\nb=2 + if a then 4+4 else 2 + 3";
-            // string testiohjelma = "fabulous a b = a + b\ny=4 + fabulous 2 3\n";
-            // string testiohjelma = "fabulous a = if a > 0 then (a + fabulous (a-1)) else 0";
-            //string testiohjelma = "f = -2-2";
-            string testiohjelma = "f x = x == 2";
+            string program = "f x y end = if y < end then f y (x+y) end else y";
 
             Console.WriteLine("Haskell-- (Haskell-minus-minus) interpreter");
         
             Hassembler hassembler = new Hassembler();
-            hassembler.ParseCode(testiohjelma);
+            hassembler.ParseCode(program);
 
             Console.WriteLine();
             Console.WriteLine("Found functions: ");
